e5168caa6e6e14730629d949bb026c0df30df85d
CAMR05 <cmoheno@centro.edu.mx>
2025-09-04 22:34:49 -0600

    creación de la esfera que representa al sol

--- Actividad-1/app.js
+++ Actividad-1/app.js
@@ -1,28 +1,72 @@
 console.log("Actividad Jardín 1. Atardecer");
-
 console.log(gsap); 
+console.log(THREE);
 
+const canvas = document.getElementById("lienzo");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-window.addEventListener("mousedown", function () {
-        gsap.to(
-            ".sol",
-            {
-                y:-900,
-                x:innerWidth/2,
-                
-                duration:3, //segundos
-                ease:"power1.out",
-                onComplete:function() {
-                    gsap.to(
-                        ".sol",
-                        {
-                            x:6700,
-                            y:80,
-                            duration:3,
-                            ease:"power1.in",
-                        }
-                    )
-                }
-            }
-        );
+//Creamos nuestros elementos básicos:
+//Escena
+const scene = new THREE.Scene();
+
+//Cámara
+//const camera = new THREE.Camera(fov, aspectRatio, near, far);
+const camera = new THREE.PerspectiveCamera(45, canvas.width/canvas.height,0.1, 1000);
+
+//Mesh
+////Geometría
+//SphereGeometry(radius, radialSegments, heightSegments);
+const geometry = new THREE.SphereGeometry();
+////Material
+const material = new THREE.
+MeshNormalMaterial({
+   flatShading: true,
 });
+
+///////// CONFIGURACIÓN DE MATCAPS
+///////// INICIO
+// Ayuda.
+// Material.
+const textureLoader = new THREE.TextureLoader();
+var matcapMaterial;
+var mesh;
+var matcapMap = textureLoader.load(
+   // Textura URL
+   './texturas/matcap2.png',
+   // on Load callback
+   function (texture) {
+       matcapMaterial = new THREE.MeshMatcapMaterial( { matcap: texture } );
+       // Mesh.
+       mesh = new THREE.Mesh( geometry, matcapMaterial );
+       // 3. Poner objeto en la escena.
+       scene.add(mesh);
+       mesh.position.z = -4;
+       // 4. Activar animación.
+       animate();
+   },
+   // on Progress (no funciona por ahora)
+   undefined,
+   // on Error callback
+   function (error) { console.error("Algo salio mal con la textura,", error); }
+);
+
+
+//////// FIN
+
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+renderer.setSize(canvas.width, canvas.height);
+
+
+//Dar instrucción de renderizar o imprimir nuestro primer frame
+renderer.render(scene, camera);
+
+// Tip para animar nuestro mesh:
+function animate() {
+   requestAnimationFrame(animate);
+   mesh.rotation.x += 0.01;
+   mesh.rotation.y += 0.01;
+   renderer.render(scene, camera);
+}
