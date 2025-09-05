865ce7951d50d07ee86bb0012b2136a2cc34b0f1
CAMR05 <cmoheno@centro.edu.mx>
2025-09-04 21:53:29 -0600

    Creación de los archivos para iniciar la actividad

---
+++ Actividad-1/app.js
@@ -0,0 +1,28 @@
+console.log("Actividad Jardín 1. Atardecer");
+
+console.log(gsap); 
+
+
+window.addEventListener("mousedown", function () {
+        gsap.to(
+            ".sol",
+            {
+                y:-900,
+                x:innerWidth/2,
+                
+                duration:3, //segundos
+                ease:"power1.out",
+                onComplete:function() {
+                    gsap.to(
+                        ".sol",
+                        {
+                            x:6700,
+                            y:80,
+                            duration:3,
+                            ease:"power1.in",
+                        }
+                    )
+                }
+            }
+        );
+});
