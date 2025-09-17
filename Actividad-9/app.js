38f691108340273c09fda4ea45a1035224c2bd22
CAMR05 <cmoheno@centro.edu.mx>
2025-09-16 23:48:21 -0600

    mejora en las posiciones del objeto con relación a lo que se observa en pantalla

--- Actividad-9/app.js
+++ Actividad-9/app.js
@@ -7,8 +7,15 @@ canvas.height = window.objHeight;
 
 
 window.addEventListener("mousedown", function () {
-    const randomX = Math.random() * window.objWidth;
-    const randomY = Math.random() * window.objHeight;
+    const objeto = this.document.querySelector(".objeto");
+    const objWidth = objeto.offsetWidth || 300;
+    const objHeight = objeto.offsetHeight || 50;
+
+    const maxX = this.window.innerWidth - objWidth;
+    const maxY = this.window.innerHeight - objHeight;
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
         gsap.to(
             ".objeto",
             {
