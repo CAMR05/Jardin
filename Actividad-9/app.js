bbd3498f38dd113ab46339ce1c7fadf4c8c3795a
CAMR05 <cmoheno@centro.edu.mx>
2025-09-16 23:41:15 -0600

    varios intentos de hacer que el objeto no salga de la pantalla y los movimientos se sientan más aleatorios

--- Actividad-9/app.js
+++ Actividad-9/app.js
@@ -2,21 +2,21 @@ console.log("Actividad Jardín 9. Posciones Randoms");
 console.log(gsap); 
 
 const canvas = document.getElementById("lienzo");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.objWidth;
+canvas.height = window.objHeight;
 
 
 window.addEventListener("mousedown", function () {
-    const randomX = Math.random() * window.innerWidth;
-    const randomY = Math.random() * window.innerHeight;
+    const randomX = Math.random() * window.objWidth;
+    const randomY = Math.random() * window.objHeight;
         gsap.to(
-            ".boomerang",
+            ".objeto",
             {
-                y:randomX,
                 x:randomX,
+                y:randomY,
                 
                 
-                duration:2, //segundos
+                duration:1, //segundos
                 ease:"power1.out",
                 onComplete:function() {
                 }
