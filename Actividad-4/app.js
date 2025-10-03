7e3e33c662afdd75e70d2c85bfe98d6980df0bd0
CAMR05 <cmoheno@centro.edu.mx>
2025-10-03 03:01:41 -0600

    se modificó el posicionamiento de las líneas y se añadió un resize para mantenerlo no importa cual sea el tamaño de la ventana

--- Actividad-4/app.js
+++ Actividad-4/app.js
@@ -14,8 +14,8 @@ ctx.lineWidth = 4;
     for (let i = 0; i < 15; i++) {
         let y = 100 + i * 20;
         let x = 50 + i * 20;
-    ctx.moveTo(400 + x, 60);
-    ctx.lineTo(400 + x, 60 + y);
+    ctx.moveTo(canvas.width/2 + x, 60);
+    ctx.lineTo(canvas.width/2 + x, 60 + y);
     }
 
     for (let i = 0; i < 15; i++) {
@@ -27,9 +27,23 @@ ctx.lineWidth = 4;
 
     for (let i = 0; i < 15; i++) {
         let y = 100 + i * 20;
-    ctx.moveTo(300, 60 + y);
-    ctx.lineTo(755, 60 + y);
+    ctx.moveTo(800, 60 + y);
+    ctx.lineTo(1300, 60 + y);
     }
 
 ctx.fill();
 ctx.stroke();
+
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+renderer.setSize(canvas.width, canvas.height);
+window.addEventListener("resize", function () {
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    renderer.setSize(canvas.width, canvas.height);
+    camera.aspect = canvas.width / canvas.height;
+    camera.updateProjectionMatrix();
+
+renderer.render(scene, camera);
+});
+renderer.render(scene, camera);
